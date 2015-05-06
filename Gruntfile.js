23d63179a6c695dc92dab425972c82e8b29a7c9a
Stuart Colville <scolville@mozilla.com>
2015-05-06 16:43:16 +0100

    Add example and publish it to gh-pages (fixes #1)

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,43 @@ module.exports = function(grunt) {
 
     clean: {
       min: ['min']
-    }
+    },
+
+    svg_sprite: {
+      dist: {
+        expand: true,
+        cwd: 'svg',
+        src: ['**/*.svg'],
+        dest: 'build',
+        options: {
+          shape: {
+            dimension: {
+              maxWidth: 125,
+              maxHeight: 125,
+            }
+          },
+          dest: "build",
+          mode: {
+            css: {
+              bust: false,
+              example: {
+                dest: 'index.html'
+              }
+            }
+          }
+        }
+      }
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'build/css',
+        message: 'Updating example',
+        repo: 'git@github.com:muffinresearch/payment-icons.git'
+      },
+      src: ['**']
+    },
+
 
   });
 
--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Payment related svg icons for use in fonts or spritesheets.
 
 The raw sketch output is in the svg directory. The minified svg output is in the min dir.
 
+## Example
+
+An example page generated with grunt-svg-sprite can be seen here: http://muffinresear.ch/payment-icons/
+
 ##  License
 
 Icon marks are copyright their respective owners.
