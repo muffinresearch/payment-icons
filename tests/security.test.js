41a9a64a0d7982e1f3c7947ccb9709269377ea73
Stuart Colville <scolville@mozilla.com>
2018-02-28 21:16:34 +0000

    Add tests

---
+++ tests/security.test.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+import cheerio from 'cheerio';
+import glob from 'glob';
+
+
+describe('Test security', () => {
+  const files = glob.sync("svg/**/*.svg");
+
+  for (var i=0; i<files.length; i++){
+    const file = files[i];
+
+    it(`should have no script ${file}`, (done) => {
+      fs.readFile(file, (err, data) => {
+        const $ = cheerio.load(data);
+        expect($('script').length).toBe(0);
+        done();
+      });
+    });
+  }
+});
